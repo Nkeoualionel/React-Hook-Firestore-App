52b9845d88902289863087f2774a927210c4070a
Lionel NKEOUA <mbembasachat9@gmail.com>
2020-08-18 22:58:43 +0000

    import timelist to app component

--- src/App.js
+++ src/App.js
@@ -2,20 +2,16 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import firebase from './firebase';
+import Timelist from './components/time-list'
 
-
-firebase.firestore().collection('test').add({
-  title: 'Wake Up',
-  time: 45
-
-})
 
 function App() {
   return (
     <div className="App">
       <h1>
-        Time reminder
+        Your Time reminder
       </h1>
+      <Timelist/>
     </div>
   );
 }
