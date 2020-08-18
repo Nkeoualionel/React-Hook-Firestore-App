9086934fa0c47cff9505b0f2bb5f8c922391d195
Lionel NKEOUA <mbembasachat9@gmail.com>
2020-08-18 22:57:50 +0000

    added variable to timelist component

--- src/components/time-list.jsx
+++ src/components/time-list.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 const timelist = () => {
     return (
+    <div>  
         <div>
             <h2>Time Liste</h2>
             <div>
@@ -15,6 +16,16 @@ const timelist = () => {
                 <option>Order by (z-a) </option>
             </select>
         </div>
+        <ol>
+            <li> 
+                <div className="time-entry"> 
+                 Your amawing title
+                  <code className="time">18 secondes</code>
+                </div>
+            </li>
+
+        </ol>
+       </div> 
     )
 }
 
