ffb556ae9df87275c45c291ee25dc1b134691447
Lionel NKEOUA <mbembasachat9@gmail.com>
2020-08-19 09:21:13 +0000

    added orderby in time list component

--- src/components/time-list.jsx
+++ src/components/time-list.jsx
@@ -2,14 +2,24 @@ import React from 'react'
 import {useEffect, useState} from 'react'
 import firebase from '../firebase';
 
-function useGetDataTime () {
+
+const SORT_OPTION =  {
+    'TIME_ASC': {column: 'secondes', direction: 'asc'},
+    'TIME_DESC': {column: 'secondes', direction: 'desc'},
+
+    'TITLE_ASC': {column: 'title', direction: 'asc'},
+    'TITLE_DESC': {column: 'title', direction: 'desc'}
+}
+
+function useGetDataTime (orderBy = 'TIME_ASC') {
 
     const [time, setTime] = useState([])
 
     useEffect(() => {
-       firebase
+      const unsuscribe = firebase
        .firestore()
        .collection('reminders')
+       .orderBy(SORT_OPTION[orderBy].column, SORT_OPTION[orderBy].direction)
        .onSnapshot((snapshot) => {
            const newTime = snapshot.docs.map((doc) => ({
                id: doc.id,
@@ -18,15 +28,19 @@ function useGetDataTime () {
 
            setTime(newTime)
        }) 
+
+       return () => unsuscribe()
        
-    }, [])
+    }, [orderBy])
 
     return time
 }
 
 const Timelist = () => {
 
-    const times =  useGetDataTime()
+    const [orderBy, setOrderBy] = useState('TIME_ASC')
+
+    const times =  useGetDataTime(orderBy)
        
     return (
     <div>  
@@ -35,12 +49,12 @@ const Timelist = () => {
             <div>
                 <label>Sort By</label>{''}
             </div>
-            <select>
-                <option>Timer (fast) </option>
-                <option>Timer (slow) </option>
+            <select value={orderBy} onChange={e => setOrderBy(e.target.value)}>
+                <option value="TIME_ASC">Timer (asc) </option>
+                <option value="TIME_DESC">Timer (desc) </option>
                 <option disabled>-------</option>
-                <option>Order by (a-z) </option>
-                <option>Order by (z-a) </option>
+                <option value="TITLE_ASC">Order by (a-z) </option>
+                <option value="TITLE_DESC">Order by (z-a) </option>
             </select>
         </div>
         <ol>
